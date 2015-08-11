96740c095997564f9f888f08fa9d8ed715382e42
karingula <karingula.v@gmail.com>
2015-08-11 12:11:10 -0500

    initial commit

---
+++ filename.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+use warnings;
+use strict;
+
+#print "Content-Type: text/html\n\n";
+
+
+use HashDB;
+use BerkeleyDB;
+my $dir = '/Users/vk2/Sites/uploads/';
+my $filename = <$dir/data.db>;
+my $dbh = new BerkeleyDB::Hash(
+        -Filename =>$filename,
+        -Flags    =>DB_CREATE)
+        or die "Error opening $filename: $! $BerkeleyDB::Error\n";
+my $key = "filename";
+my $value;
+my $status = $dbh->db_get($key,$value);
+print $status;
+if ($status) {
+    print "Error: $status\n";
+}
+
+else {
+    print "$value";
+}
+
+#HashDB::delete_berkeley($dir);
